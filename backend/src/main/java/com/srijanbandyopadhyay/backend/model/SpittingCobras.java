fe46356ce434e937a11addf58e20f838e66f319e
srijanishere <banerjee.srijan2001@gmail.com>
2025-02-18 17:58:17 +0530

    model classes updated

---
+++ backend/src/main/java/com/srijanbandyopadhyay/backend/model/SpittingCobras.java
@@ -0,0 +1,35 @@
+package com.srijanbandyopadhyay.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class SpittingCobras
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String role;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
