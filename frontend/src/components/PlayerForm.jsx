68df21e758849219511fb3043a4c624c565b573c
srijanishere <banerjee.srijan2001@gmail.com>
2025-02-19 01:02:30 +0530

    updated UI elements

--- frontend/src/components/PlayerForm.jsx
+++ frontend/src/components/PlayerForm.jsx
@@ -11,6 +11,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import Tooltip from '@mui/joy/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,7 +35,18 @@ export default function AccordionUsage() {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
   const [team, setTeam] = useState(0);
-  const [players, setPlayers] = useState([]);
+  const [bandits, setBandits] = useState([]);
+  const [bull, setBulls] = useState([]);
+  const [cobras, setCobras] = useState([]);
+  const [stallions, setStallions] = useState([]);
+  const [royals, setRoyals] = useState([]);
+  const [banditsAmount, setBanditsAmount] = useState(10000);
+  const [bullsAmount, setBullsAmount] = useState(10000);
+  const [cobrasAmount, setCobrasAmount] = useState(10000);
+  const [stallionsAmount, setStallionsAmount] = useState(10000);
+  const [royalsAmount, setRoyalsAmount] = useState(10000);
+  const [amount, setAmount] = useState(0);
+
 
   // function to handle button click and add a player
   const addPlayer = (e) => {
@@ -41,6 +61,7 @@ export default function AccordionUsage() {
       }).then(() => {
         alert("Player added successfully!");
       });
+      setBanditsAmount(banditsAmount - amount);
     } else if (team === 2) {
       fetch("http://localhost:8080/bcl/bulls/add", {
         method: "POST",
@@ -49,6 +70,7 @@ export default function AccordionUsage() {
       }).then(() => {
         alert("Player added successfully!");
       });
+      setBullsAmount(bullsAmount - amount);
     } else if (team === 3) {
       fetch("http://localhost:8080/bcl/royals/add", {
         method: "POST",
@@ -57,6 +79,7 @@ export default function AccordionUsage() {
       }).then(() => {
         alert("Player added successfully!");
       });
+      setRoyalsAmount(royalsAmount - amount);
     } else if (team === 4) {
       fetch("http://localhost:8080/bcl/cobras/add", {
         method: "POST",
@@ -65,6 +88,7 @@ export default function AccordionUsage() {
       }).then(() => {
         alert("Player added successfully!");
       });
+      setCobrasAmount(cobrasAmount - amount);
     } else if (team === 5) {
       fetch("http://localhost:8080/bcl/stallions/add", {
         method: "POST",
@@ -73,17 +97,16 @@ export default function AccordionUsage() {
       }).then(() => {
         alert("Player added successfully!");
       });
-    }
-    else
-    {
-        alert("Invalid team choice!");
+      setStallionsAmount(stallionsAmount - amount);
+    } else {
+      alert("Invalid team choice!");
     }
   };
 
   // function to handle a button click for clearing the form fields
   const clearForm = () => {
     document.getElementById("player-form").reset();
-  }
+  };
 
   return (
     <div style={{ margin: "15px", padding: "5px" }}>
@@ -99,7 +122,12 @@ export default function AccordionUsage() {
         </AccordionSummary>
         <AccordionDetails>
           <Container style={{ color: "white" }}>
-            <form className={classes.root} id="player-form" noValidate autoComplete="off">
+            <form
+              className={classes.root}
+              id="player-form"
+              noValidate
+              autoComplete="off"
+            >
               <TextField
                 id="outlined-basic"
                 label="Player Name"
@@ -112,6 +140,13 @@ export default function AccordionUsage() {
                 label="Player Role"
                 variant="outlined"
                 onChange={(e) => setRole(e.target.value)}
+                fullWidth
+              />
+              <TextField
+                id="outlined-basic"
+                label="Player Amount"
+                variant="outlined"
+                onChange={(e) => setAmount(Number(e.target.value))}
                 fullWidth
               />
               <Select
@@ -130,7 +165,7 @@ export default function AccordionUsage() {
           </Container>
         </AccordionDetails>
         <AccordionActions>
-        <Button
+          <Button
             style={{ margin: "10px", padding: "5px" }}
             variant="contained"
             onClick={clearForm}
@@ -143,6 +178,136 @@ export default function AccordionUsage() {
             onClick={addPlayer}
           >
             ADD
+          </Button>
+        </AccordionActions>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ArrowDownwardIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography component="span">
+            <h3>Auction Sheet</h3>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Stack spacing={5} useFlexGap>
+            <Stack direction="row" spacing={2}>
+              <TableContainer component={Paper}>
+                <h4>Benrec Bandits</h4>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <caption>
+                    <Tooltip title="Delete" variant="plain">
+                      <Button variant="plain">Total : {banditsAmount}</Button>
+                    </Tooltip>
+                  </caption>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell style={{ fontWeight: "bolder" }}>
+                        Player
+                      </TableCell>
+                      <TableCell align="right" style={{ fontWeight: "bolder" }}>
+                        Role
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody></TableBody>
+                </Table>
+              </TableContainer>
+              <TableContainer component={Paper}>
+                <h4>Benrec Big Bulls</h4>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <caption>
+                    <Tooltip title="Delete" variant="plain">
+                      <Button variant="plain">Total : {bullsAmount}</Button>
+                    </Tooltip>
+                  </caption>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell style={{ fontWeight: "bolder" }}>
+                        Player
+                      </TableCell>
+                      <TableCell align="right" style={{ fontWeight: "bolder" }}>
+                        Role
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody></TableBody>
+                </Table>
+              </TableContainer>
+            </Stack>
+            <Stack direction="row" spacing={2}>
+              <TableContainer component={Paper}>
+                <h4>Benrec Royals</h4>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <caption>
+                    <Tooltip title="Delete" variant="plain">
+                      <Button variant="plain">Total : {royalsAmount}</Button>
+                    </Tooltip>
+                  </caption>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell style={{ fontWeight: "bolder" }}>
+                        Player
+                      </TableCell>
+                      <TableCell align="right" style={{ fontWeight: "bolder" }}>
+                        Role
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody></TableBody>
+                </Table>
+              </TableContainer>
+              <TableContainer component={Paper}>
+                <h4>Benrec Spitting Cobras</h4>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <caption>
+                    <Tooltip title="Delete" variant="plain">
+                      <Button variant="plain">Total : {cobrasAmount}</Button>
+                    </Tooltip>
+                  </caption>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell style={{ fontWeight: "bolder" }}>
+                        Player
+                      </TableCell>
+                      <TableCell align="right" style={{ fontWeight: "bolder" }}>
+                        Role
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody></TableBody>
+                </Table>
+              </TableContainer>
+            </Stack>
+            <TableContainer component={Paper}>
+              <h4>Benrec Stallion</h4>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <caption>
+                    <Tooltip title="Delete" variant="plain">
+                      <Button variant="plain">Total : {stallionsAmount}</Button>
+                    </Tooltip>
+                  </caption>
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ fontWeight: "bolder" }}>Player</TableCell>
+                    <TableCell align="right" style={{ fontWeight: "bolder" }}>
+                      Role
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody></TableBody>
+              </Table>
+            </TableContainer>
+          </Stack>
+        </AccordionDetails>
+        <AccordionActions>
+          <Button
+            style={{ margin: "10px", padding: "5px" }}
+            variant="contained"
+          >
+            END AUCTION
           </Button>
         </AccordionActions>
       </Accordion>
